8e50ad2f1638df4782494254144991571f72f4c1
psztyvivien <ptyvivi04@icloud.com>
2025-05-01 11:25:00 +0200

    update program.cs

--- BACKEND/billapi/billapi/Program.cs
+++ BACKEND/billapi/billapi/Program.cs
@@ -1,6 +1,17 @@
+using billapi.Controllers;
+//using billapi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IBillRepository, BillRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
 
 app.Run();
