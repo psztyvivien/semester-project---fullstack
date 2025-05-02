dadc5ecc55e84f10a5fda8b3c78fb3220027daa5
psztyvivien <ptyvivi04@icloud.com>
2025-05-02 21:21:07 +0200

    delete rendertable func

--- FRONTEND/app.js
+++ FRONTEND/app.js
@@ -87,29 +87,3 @@ function convertTextToNumber(text) {
 
     return 0;
 }
-
-function renderTable() {
-    const latestName = payments[payments.length - 1].name;
-    let html = `
-    <table border="1" cellpadding="5">
-        <thead>
-            <tr>
-                <th>Név</th><th>Összeg (szám)</th><th>Összeg (szöveg)</th><th>Dátum</th>
-            </tr>
-        </thead>
-        <tbody>
-    `;
-
-    payments.forEach(p => {
-        html += `
-        <tr>
-            <td>${p.name === latestName ? `<span class="badge" style="color: white; background-color: red; padding: 2px 5px; border-radius: 5px;">${p.name}</span>` : p.name}</td>
-            <td>${p.amountNumber}</td>
-            <td>${p.amountText}</td>
-            <td>${p.date}</td>
-        </tr>`;
-    });
-
-    html += `</tbody></table>`;
-    document.getElementById('result').innerHTML = html;
-}
