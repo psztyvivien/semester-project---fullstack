718b13ab4a4ffc9760424d68f4d10a9a25c1b527
psztyvivien <ptyvivi04@icloud.com>
2025-05-02 21:19:35 +0200

    add downloadAndDisplay func

--- FRONTEND/app.js
+++ FRONTEND/app.js
@@ -1,5 +1,60 @@
 const payments = [];
 
+async function donwloadAndDisplay(){
+    const response = await fetch('http://localhost:5500/billapi')
+    const bills = await response.json()
+    console.log(bills)
+
+    document.querySelector('#bills').innerHTML = ''
+    payments = []
+
+    bills.map(x => {
+        payments.push(x)
+
+        let tr = document.createElement('tr')
+        let tdName = document.createElement('td')
+        let tdAmountNumber = document.createElement('td')
+        let tdAmountText = document.createElement('td')
+        let tdDate = document.createElement('td')
+        let tdActions = document.createElement('td')
+
+        tdID.innerHTML =  x.id
+        tdName.innerHTML = x.name
+        tdAmountNumber.innerHTML = x.amountNumber
+        tdAmountText.innerHTML = x.amountText
+        tdDate.innerHTML = x.date
+
+        tr.appendChild(tdID)
+        tr.appendChild(tdName)
+        tr.appendChild(tdAmountNumber)
+        tr.appendChild(tdAmountText)
+        tr.appendChild(tdDate)
+        tr.appendChild(tdActions)
+
+        let btnUpdate = document.createElement('button')
+        btnUpdate.classList.add('btn')
+        btnUpdate.classList.add('btn-sm')
+        btnUpdate.classList.add('btn-warning')
+        btnUpdate.classList.add('mx-2')
+        btnUpdate.innerHTML = 'Update'
+        btnUpdate.idParameter = x.id
+        btnUpdate.addEventListener('click', updateLog)
+        tdActions.appendChild(btnUpdate)
+
+        let btnDel = document.createElement('button')
+        btnDel.classList.add('btn')
+        btnDel.classList.add('btn-sm')
+        btnDel.classList.add('btn-danger')
+        btnDel.classList.add('mx-2')
+        btnDel.innerHTML = 'Delete'
+        btnDel.idParameter = x.id
+        btnDel.addEventListener('click', deleteLog)
+        tdActions.appendChild(btnDel)
+
+        document.querySelector('#bills').appendChild(tr)
+    })
+}
+
 document.getElementById('payment-form').addEventListener('submit', function (e) {
     e.preventDefault();
 
